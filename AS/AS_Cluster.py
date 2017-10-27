Fix bugs in dpocket generation calculation Updated demo

AS/AS_Cluster.py
@@ -21,7 +21,8 @@ class AS_Cluster(object):
         # Parent receptor attribute reference
         self.parent = receptor
         self.receptor_top = receptor.top
-        self.receptor_snapshot = self.parent.trajectory[snapshot_idx]
+
+        self.universe = receptor.parent
         self.config = receptor.config
         self.is_polar = receptor.is_polar
 
@@ -37,21 +38,40 @@ class AS_Cluster(object):
         # Link pocket name change to trajectory object
 
         # Initialize storage array
-        self.contact = np.empty(self.n_alphas, int)
-        self.polar_score = np.empty(self.n_alphas, float)
-        self.nonpolar_score = np.empty(self.n_alphas, float)
-        self.total_score = np.empty(self.n_alphas, float)
+        self._contact = np.empty(self.n_alphas, int)
+
+        self._polar_score = np.empty(self.n_alphas, float)
+        self._nonpolar_score = np.empty(self.n_alphas, float)
+        self._total_score = np.empty(self.n_alphas, float)
 
         self._tessellation(self.config)
-        if self.parent.structure_type == 0:
-            self.oppsite_struct = self.parent.parent.binder
-        elif self.parent.structure_type == 1:
-            self.oppsite_struct = self.parent.parent.receptor
-        else:
-            self.oppsite_struct = None
 
     def __repr__(self):
-        return "Alpha Atom cluster of #{} frame".format(self.snapshot_idx)
+        return "Alpha Atom cluster of #{} frame, {} pockets, {} Alpha Atoms".format(self.snapshot_idx,self.n_pockets,self.n_alphas)
+
+    @property
+    def receptor_snapshot(self):
+        return self.parent.trajectory[self.snapshot_idx]
+
+    @property
+    def n_alphas(self):
+        return self.top.n_atoms
+
+    @property
+    def n_pockets(self):
+        return self.top.n_residues
+
+    @property
+    def alphas(self):
+        for atom in self.top._atoms:
+            yield atom
+
+    @property
+    def pockets(self):
+        return self.top.residues
+
+    def pocket(self,index):
+        return self.top.residue(index)
 
     def alpha(self, i):
         return self.top.atom(i)
@@ -118,17 +138,17 @@ class AS_Cluster(object):
         self.traj.xyz = np.expand_dims(filtered_alpha_xyz, axis=0)
         filtered_lining_xyz = np.take(self.receptor_snapshot.xyz[0], self.alpha_lining, axis=0)
         # calculate the polarity of alpha atoms
-        self.total_score = np.array([getTetrahedronVolume(i) for i in filtered_lining_xyz])
+        self._total_score = np.array([getTetrahedronVolume(i) for i in filtered_lining_xyz])
 
         pocket_sasa = np.take(self._get_SASA(), self.alpha_lining)
 
         polar_ratio = np.average(np.take(self.is_polar, self.alpha_lining), axis=1, weights=pocket_sasa)
 
-        self.polar_score = self.total_score * polar_ratio
+        self._polar_score = self._total_score * polar_ratio
 
-        self.nonpolar_score = self.total_score - self.polar_score
+        self._nonpolar_score = self._total_score - self._polar_score
 
-        self.contact = np.zeros(self.top.n_atoms, dtype=int)
+        self._contact = np.zeros(self.top.n_atoms, dtype=int)
 
         # centoids = []
         # for pocket in self.pockets:
@@ -153,6 +173,8 @@ class AS_Cluster(object):
         joined_traj = self.parent.trajectory[self.snapshot_idx].stack(self.traj)
         parent_sasa = shrake_rupley(self.parent.trajectory[self.snapshot_idx])[0]
 
+        # todo fix the issue where too close of two alpha atoms cause shrake_rupley to crush
+
         joined_sasa = shrake_rupley(joined_traj, change_radii={'VS': 0.17})[0][:len(parent_sasa)]
         return parent_sasa - joined_sasa
 
@@ -163,18 +185,18 @@ class AS_Cluster(object):
         :return: np.array
         """
         if binder_traj is None:
-            binder_traj = self.oppsite_struct.traj
+            binder_traj = self.universe.binder.traj
         contact_matrix = getContactMatrix(self.traj.xyz[0], binder_traj.xyz[0],
                                           threshold=self.config.contact_threshold / 10)
-        self.contact = np.array(np.any(contact_matrix, axis=1))
-        return self.contact
+        self._contact = np.array(np.any(contact_matrix, axis=1))
+        return self._contact
 
     def _get_contact_space(self):
         """
         :return array
         """
-        self._get_contact_list(self.oppsite_struct.traj)
-        contact_space = self.contact * self.total_score
+        self._get_contact_list(self.universe.binder.traj)
+        contact_space = self._contact * self._total_score
         return contact_space
 
     def _slice(self, new_alpha_indices: list) -> None:
@@ -191,10 +213,10 @@ class AS_Cluster(object):
 
 
         self.alpha_lining = np.take(self.alpha_lining, new_alpha_indices,axis=0)
-        self.contact = np.take(self.contact, new_alpha_indices, axis=0)
-        self.total_score = np.take(self.total_score, new_alpha_indices, axis=0)
-        self.polar_score = np.take(self.polar_score, new_alpha_indices, axis=0)
-        self.nonpolar_score = np.take(self.nonpolar_score, new_alpha_indices, axis=0)
+        self._contact = np.take(self._contact, new_alpha_indices, axis=0)
+        self._total_score = np.take(self._total_score, new_alpha_indices, axis=0)
+        self._polar_score = np.take(self._polar_score, new_alpha_indices, axis=0)
+        self._nonpolar_score = np.take(self._nonpolar_score, new_alpha_indices, axis=0)
 
         for pocket_idx, pocket in enumerate(self.pockets):
             pocket.cluster = self
@@ -222,7 +244,7 @@ class AS_Cluster(object):
         # lining_atom_idx = np.take(self.alpha_atom_lining, index_list).flatten()
 
         lining_atom = [self.receptor_top.atom(i) for i in self._get_lining_atoms(index_list)]
-        lining_residue_idx = [atom.residue.index for atom in lining_atom]
+        lining_residue_idx = [atom.residue.index for atom  in lining_atom]
 
         return np.unique(lining_residue_idx)
 
@@ -233,25 +255,6 @@ class AS_Cluster(object):
         xyz = np.take(self.traj.xyz[0], cluster, axis=0)
         return np.mean(xyz, axis=0)
 
-    @property
-    def n_alphas(self):
-        return self.top.n_atoms
-
-    @property
-    def n_pockets(self):
-        return self.top.n_residues
-
-    @property
-    def alphas(self):
-        for atom in self.top._atoms:
-            yield atom
-
-    @property
-    def pockets(self):
-        return self.top.residues
-
-    def pocket(self,index):
-        return self.top.residue(index)
 
 class AS_DPocket:
     def __init__(self, universe):

AS/AS_Universe.py
@@ -27,8 +27,7 @@ class AS_Universe(object):
         self.set_binder(binder)
 
         if guess_receptor_binder:
-            self.guess_receptor_binder(receptor,guess_by_order)
-
+            self.guess_receptor_binder(receptor, guess_by_order)
 
         self.others = None
         self.view = None
@@ -39,8 +38,22 @@ class AS_Universe(object):
                                                                                           self.binder.n_residues,
                                                                                           self.binder.n_atoms)
 
+    @property
+    def clusters(self):
+        """
+        return list of clusters
+        :return: list
+        """
+        return self.receptor.clusters
 
+    @property
+    def n_frames(self):
+        if self.receptor is None:
+            return 0
+        else:
+            return self.receptor.trajectory.n_frames
 
+    @property
     def n_atoms(self) -> int:
         """
         return the total number of atoms in receptor and binders
@@ -71,7 +84,6 @@ class AS_Universe(object):
     def pockets(self, snapshot_idx):
         return self.receptor._clusters[snapshot_idx].pockets
 
-
     def cluster(self, snapshot_idx: int = 0) -> AS_Cluster:
         """
         return list of clusters
@@ -79,21 +91,6 @@ class AS_Universe(object):
         :return: object, AS_Cluster
         """
         return self.receptor._clusters[snapshot_idx]
-
-    @property
-    def clusters(self):
-        """
-        return list of clusters
-        :return: list
-        """
-        return self.receptor.clusters
-
-    @property
-    def n_frames(self):
-        if self.receptor is None:
-            return 0
-        else:
-            return self.receptor.trajectory.n_frames
 
     def guess_receptor_binder(self, traj, by_order: bool = True) -> bool:
         """
@@ -104,7 +101,7 @@ class AS_Universe(object):
         :return: bool, if any macro molecule were found.
         """
         if traj is None:
-            return False
+            raise Exception("Cannot guess receptor and binder, no structure detected")
         molecule_list = []
         # remove solvent and divide into molecules. This will guess which one is the
         for molecule in traj.topology.find_molecules():
@@ -130,24 +127,23 @@ class AS_Universe(object):
         """
         set binder (ligand) in session
         :param structure: object, trajectory
+        :param append: Bool, if the new binder should be appended to the preview one, default overwritten.
         :return:
         """
         if structure is None:
             self.binder = None
             return
-
         if append and (self.binder is not None):
             x = self.binder.trajectory + structure
             self.binder.trajectory = x
-
         else:
             self.binder = AS_Structure(structure, structure_type=1, parent=self)
 
     def set_receptor(self, structure: object, append=False):
         """
         set receptor (protein) in session
-        :param receptor: object, trajectory
-        :param append: bool
+        :param structure: object, trajectory
+        :param append: Bool, if the new binder should be appended to the preview one, default overwritten.
         :return:
         """
         if structure is None:
@@ -160,8 +156,6 @@ class AS_Universe(object):
 
         else:
             self.receptor = AS_Structure(structure, structure_type=0, parent=self)
-
-
 
     def set_others(self, others: object):
         """
@@ -193,34 +187,8 @@ class AS_Universe(object):
         self.receptor.clusters[snapshot_idx].screen_by_contact()
 
     def get_pockets(self, snapshot_idx: int = 0) -> list:
-        return self.receptor.clusters[snapshot_idx].pockets
 
-    """
-    Visualization methods
-    """
-
-    def view_snapshot(self, snapshot_idx: int = 0) -> object:
-        self.show_receptor(snapshot_idx)
-        self.show_binder(snapshot_idx)
-        self.show_pocket(snapshot_idx)
-        return self.view
-
-    def show_receptor(self, snapshot_idx=0):
-        self.view = nv.show_mdtraj(self.receptor.trajectory[snapshot_idx], gui=True)
-        self.receptor_view = self.view.component_0
-        self.receptor_view.clear_representations()
-        self.receptor_view.add_surface(selection='protein', opacity=1, color='white')
-
-    def show_pocket_label(self):
-        self.view.component_2.add_representation(repr_type='label', lableType='residueindex',color = 'residueindex')
-
-    def show_binder(self, snapshot_idx=0):
-        self.binder_view = self.view.add_trajectory(self.binder.trajectory[snapshot_idx])
-
-    def show_pocket(self, snapshot_idx=0):
-        self.pocket_view = self.view.add_trajectory(self.receptor.cluster(snapshot_idx).traj)
-        self.pocket_view.clear_representations()
-        self.pocket_view.add_representation(repr_type='ball+stick', selection='all', color='residueindex')
+        return self.pockets(snapshot_idx)
 
     def _get_face_atoms(self, snapshot_idx=0):
         """
@@ -244,13 +212,14 @@ class AS_Universe(object):
         return set(interface_atom_index)
 
     def screen_pockets(self):
-
-        if self.config.screen_by_ligand_contact:
+        if self.receptor.n_clusters < 1:
+            raise Exception("Must process all frames before screening pockets.")
+        if self.config.screen_by_lig_cntct:
             for snapshot_idx in range(self.n_frames):
                 contact = self.cluster(snapshot_idx)._get_contact_list()
                 self.cluster(snapshot_idx)._slice(np.where(contact != 0)[0])
 
-        if self.config.screen_by_face and not self.config.screen_by_ligand_contact:
+        if self.config.screen_by_face and not self.config.screen_by_lig_cntct:
             for snapshot_idx in range(self.n_frames):
                 face_pocket_alpha_index = set()
                 face_atoms_index = self._get_face_atoms(snapshot_idx)
@@ -276,6 +245,35 @@ class AS_Universe(object):
                     if pocket.get_lining_residues:
                         face_pocket_alpha_index.union(set(pocket.get_alpha_index))
                 cluster._slice(face_pocket_alpha_index)
+
+    """
+    Visualization methods
+    """
+
+    def view_snapshot(self, snapshot_idx: int = 0) -> object:
+        self.show_receptor(snapshot_idx)
+        self.show_binder(snapshot_idx)
+        self.show_pocket(snapshot_idx)
+        return self.view
+
+    def show_receptor(self, snapshot_idx=0):
+        self.view = nv.show_mdtraj(self.receptor.trajectory[snapshot_idx], gui=True)
+        self.receptor_view = self.view.component_0
+        self.receptor_view.clear_representations()
+        self.receptor_view.add_surface(selection='protein', opacity=1, color='white')
+
+    def show_pocket_label(self):
+        self.view.component_2.add_representation(repr_type='label', lableType='residueindex', color='residueindex')
+
+    def show_binder(self, snapshot_idx=0):
+        self.binder_view = self.view.add_trajectory(self.binder.trajectory[snapshot_idx])
+
+    def show_pocket(self, snapshot_idx=0):
+        self.pocket_view = self.view.add_trajectory(self.receptor.cluster(snapshot_idx).traj)
+        self.pocket_view.clear_representations()
+        self.pocket_view.add_representation(repr_type='ball+stick', selection='all', color='residueindex')
+
+
 
 
 if __name__ == '__main__':