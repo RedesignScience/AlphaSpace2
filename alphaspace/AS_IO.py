ccb0d77b930157676915d7578853991071507e9d
Haotian_Li <lenhsherr@gmail.com>
2017-11-14 14:15:15 -0500

    save and load from pickle binary file

--- alphaspace/AS_IO.py
+++ alphaspace/AS_IO.py
@@ -1,3 +1,4 @@
+import pickle
 def write_pocket_pdb(pocket_obj, file_obj):
     for alpha in pocket_obj.alphas:
         line = get_pdb_line('ATOM', alpha.index, 'AAO', 'AAC', 'H', pocket_obj.index, alpha.xyz[0], alpha.xyz[1],
@@ -6,9 +7,11 @@ def write_pocket_pdb(pocket_obj, file_obj):
     return
 
 
-def get_pdb_line(atom='ATOM', atomnum=0, atomname='', resname='', chain_idx='', resnum=0, x=0.0, y=0.0, z=0.0, occ=0.0,
-                 bfactor=0.0, element=''):
-    j = [atom, atomnum, atomname, resname, chain_idx, resnum, x, y, z, occ, bfactor, element]
+def get_pdb_line(atom='ATOM', atomnum=0, atomname=' ', resname=' ', chain_idx=' ', resnum=0, x=0.0, y=0.0, z=0.0,
+                 occ=0.0,
+                 bfactor=0.0, element=' '):
+    j = [str(atom), str(int(atomnum)), str(atomname), str(resname), str(chain_idx), str(int(resnum)), float(x),
+         float(y), float(z), float(occ), float(bfactor), str(element)]
     j[0] = j[0].ljust(6)  # atom#6s
     j[1] = j[1].rjust(5)  # aomnum#5d
     j[2] = j[2].center(4)  # atomname$#4s
@@ -21,8 +24,27 @@ def get_pdb_line(atom='ATOM', atomnum=0, atomname='', resname='', chain_idx='', 
     j[9] = str('%6.2f' % (j[9])).rjust(6)  # occ
     j[10] = str('%6.2f' % j[10]).ljust(6)  # temp
     j[11] = j[11].rjust(12)  # elname
-    line = (
-        "{}{} {} {} {}{}    {}{}{}{}{}{}\n".format(j[0], j[1], j[2], j[3], j[4], j[5], j[6], j[7], j[8], j[9], j[10],
-                                                   j[11]))
+    line = ("{}{} {} {} {}{}    {}{}{}{}{}{}".format(
+        j[0], j[1], j[2], j[3], j[4], j[5], j[6], j[7], j[8], j[9], j[10], j[11]))
+
+
 
     return line
+
+
+def save_universe(universe, file_path: str) -> bool:
+    try:
+        with open(file_path, 'wb') as handle:
+            pickle.dump(universe, handle)
+        return True
+    except:
+        print('Cannot save to {}'.format(file_path))
+        return False
+
+
+def load_universe(file_path: str):
+    try:
+        with open(file_path, 'rb') as handle:
+            return pickle.load(handle)
+    except:
+        print('Cannot load from {}'.format(file_path))
--- bin/run_alphaspace.py
+++ bin/run_alphaspace.py
@@ -1,6 +1,7 @@
 import mdtraj
 
 import alphaspace
+from alphaspace.AS_IO import get_pdb_line
 
 
 def run_alphaspace(pdb):
@@ -10,8 +11,16 @@ def run_alphaspace(pdb):
 
     universe.screen_pockets()
 
+    "ATOM      1  N   GLU     1      10.801 -12.147  -5.180  1.00  0.00"
+    "ATOM       alpha_idx  A   AAC     Pocket_idx      xyz  polar  nonpolar polar_occupy_rate nonpolar_occupy_rate"
+    "ATOM       pocket_center  A   ACC     Pocket_idx      xyz  polar  nonpolar polar_occupy_rate nonpolar_occupy_rate"
     for pocket in universe.pockets():
-        print(len(pocket.alpha_idx), pocket.get_polar_score(), pocket.get_nonpolar_score())
+        for alpha in pocket.alphas:
+            print(get_pdb_line(atomnum=alpha.idx, atomname='A', resname="AAC", resnum=pocket._idx, x=alpha.xyz[0],
+                               y=alpha.xyz[0], z=alpha.xyz[0], occ=alpha.polar_score, bfactor=alpha.nonpolar_score))
+
+        print(get_pdb_line(atomnum=alpha.idx, atomname='A', resname="AAC", resnum=pocket._idx, x=alpha.xyz[0],
+                           y=alpha.xyz[0], z=alpha.xyz[0], occ=alpha.polar_score, bfactor=alpha.nonpolar_score))
 
     return
 
