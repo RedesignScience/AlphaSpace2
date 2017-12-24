5a33e7afd7a20f9c13d37171347aac8ac6918e04
Haotian_Li <lenhsherr@gmail.com>
2017-12-24 18:08:43 -0500

    doc update

--- alphaspace/__init__.py
+++ alphaspace/__init__.py
@@ -1,20 +1,4 @@
 
-try:
-    import numpy as np
-except:
-    print("Numpy is needed")
-    exit()
-
-try:
-    import scipy
-except:
-    print("scipy is needed")
-    exit()
-try:
-    import mdtraj
-except:
-    print("mdtraj is needed")
-    exit()
 
 try:
     import nglview as nv
